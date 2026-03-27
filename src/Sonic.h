c6bb5ac417e536c18058a08b95e93993222a82d9
Axxel-otl <Axxel.Inkordia@proton.me>
2026-03-27 15:27:17 -0600

    new file:   examples/MultiSensor.ino/MultiSensor.ino 	modified:   library.properties 	modified:   src/Sonic.cpp 	modified:   src/Sonic.h

--- src/Sonic.h
+++ src/Sonic.h
@@ -1,26 +1,34 @@
-/*  
-    * Sonic Library - Ultrasonic Sensor HC-SR04  
-    * Copyright (c) 2026 Jorge Leonardo Mendoza Hernández
-    * MIT License  
-*/
-
-#ifndef SONIC_H
-#define SONIC_H
-#include <Arduino.h>
-
-class Sonic {
-    public:
-        Sonic();
-        void attach(uint8_t trig, uint8_t echo);
-        long scream(int limMin = 3, int limMax = 800);
-        long average(int times = 2, int limMin = 3, int limMax = 800);
-    private:
-        uint8_t trig;
-        uint8_t echo;
-        volatile long start;
-        volatile long dist;
-        volatile bool ready;
-        volatile bool measuring;
-};
-
+/*    
+    * Sonic Library - Ultrasonic Sensor HC-SR04    
+    * Copyright (c) 2026 Jorge Leonardo Mendoza Hernández  
+    * MIT License    
+*/  
+  
+#ifndef SONIC_H  
+#define SONIC_H  
+#include <Arduino.h>  
+  
+class Sonic {  
+    public:  
+        Sonic();  
+        void attach(uint8_t trig, uint8_t echo);  
+        long scream(int limMin = 3, int limMax = 800);  
+        long average(int times = 2, int limMin = 3, int limMax = 800);  
+        static void cleanup();  // Optional cleanup method  
+    private:  
+        uint8_t trig;  
+        uint8_t echo;  
+        volatile long start;  
+        volatile long dist;  
+        volatile bool ready;  
+        volatile bool measuring;  
+          
+        // Auto-scaling static members  
+        static Sonic** sensorInstances;  
+        static uint8_t* sensorPins;  
+        static uint8_t sensorCount;  
+        static uint8_t maxSensors;  
+        static uint8_t totalObjectsCreated;  
+};  
+  
 #endif
