8ce37743f483b5b690ed183ea972963c66096b0b
Axxel-otl <Axxel.Inkordia@proton.me>
2026-04-10 14:58:47 -0600

    Change license from MIT to GNU GPL v3
    Updated license information from MIT to GNU GPL v3.

--- src/Sonic.cpp
+++ src/Sonic.cpp
@@ -1,8 +1,20 @@
-/*  
-    * Sonic Library - Ultrasonic Sensor HC-SR04  
-    * Copyright (c) 2026 Jorge Leonardo Mendoza Hernández
-    * MIT License  
-*/
+/*
+ * Sonic Library - Ultrasonic Sensor HC-SR04
+ * Copyright (c) 2026 Jorge Leonardo Mendoza Hernández
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "Sonic.h"
 
