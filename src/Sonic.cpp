7cfcbbe189a25c95a66d844fba23a17a9182cf88
Axxel-otl <Axxel.Inkordia@proton.me>
2026-04-13 18:12:39 -0600

    Merge branch 'main' of https://github.com/Axxel-otl/Sonic

--- src/Sonic.cpp
+++ src/Sonic.cpp
@@ -1,74 +1,125 @@
 /*
-    * Sonic Library - Ultrasonic Sensor HC-SR04  
+    * Sonic Library - Ultrasonic Sensor HC-SR04 & IR Analog Sensor
     * Copyright (c) 2026 Jorge Leonardo Mendoza Hernández
-    * MIT License  
+    * 
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    * 
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU General Public License for more details.
+    * 
+    * You should have received a copy of the GNU General Public License
+    * along with this program. If not, see <https://gnu.org>.
 */
 
 #include "Sonic.h"
 
-Sonic::Sonic(){
+Sonic* Sonic::firstInstance = nullptr;
 
+Sonic::Sonic() {
+    this->next = nullptr;
+    this->ready = false;
+    this->measuring = false;
+    this->dist = 0;
 }
 
-static Sonic* instance = nullptr;
-
-void Sonic::attach(uint8_t trig, uint8_t echo){
-    instance = this;
+void Sonic::attach(uint8_t trig, uint8_t echo) {
     this->trig = trig;
-    pinMode(this->trig, OUTPUT);
     this->echo = echo;
+    pinMode(this->trig, OUTPUT);
     pinMode(this->echo, INPUT);
+    this->next = firstInstance;
+    firstInstance = this;
+    attachInterrupt(digitalPinToInterrupt(this->echo), Sonic::globalISR, CHANGE);
 }
 
-long Sonic::scream(int limMin, int limMax){
-    long dist = this->dist;
-    if(ready){
+void Sonic::globalISR() {
+    Sonic* current = firstInstance;
+    while (current != nullptr) {
+        int state = digitalRead(current->echo);
+        if (state == HIGH && !current->ready) {
+            current->start = micros();
+        } else if (state == LOW && current->measuring) {
+            current->dist = (micros() - current->start) / 58;
+            current->ready = true;
+            current->measuring = false;
+        }
+        current = current->next;
+    }
+}
+
+long Sonic::scream(int limMin, int limMax) {
+    if (ready) {
         ready = false;
-        if(dist >= limMin && dist <= limMax){
-            return(dist);
-        } else{
-            return(-1);
+        long lastDist = dist;
+        if (lastDist >= limMin && lastDist <= limMax) {
+            return lastDist;
         }
+        return -1;
     }
-    if(!measuring){
-        digitalWrite(this->trig, LOW);
+    if (!measuring) {
+        digitalWrite(trig, LOW);
         delayMicroseconds(2);
-        digitalWrite(this->trig, HIGH);
+        digitalWrite(trig, HIGH);
         delayMicroseconds(10);
-        digitalWrite(this->trig, LOW);
+        digitalWrite(trig, LOW);
         measuring = true;
-        attachInterrupt(digitalPinToInterrupt(this->echo), isr, CHANGE);
     }
-    return(-2);
+    return -2;
 }
 
-void isr(){
-    if(digitalRead(instance->echo) == HIGH){
-        instance->start = micros();
-    } else{
-        instance->dist = (micros()-instance->start)/58;
-        detachInterrupt(digitalPinToInterrupt(instance->echo));
-        instance->ready = true;
-        instance->measuring = false;
-    }
-}
-
-long Sonic::average(int times, int limMin, int limMax){
-    long dist = 0;
+long Sonic::average(int times, int limMin, int limMax) {
+    long totalDist = 0;
     int validTries = 0;
-    for(int i = 0; i < times; i++){
+    for (int i = 0; i < times; i++) {
         long valid = -2;
-        while(valid == -2){
+        uint32_t timeout = millis();
+        while (valid == -2) {
             valid = scream(limMin, limMax);
+            if (millis() - timeout > 100) break;
         }
-        if(valid != -1){
-            dist += valid;
+        if (valid >= 0) {
+            totalDist += valid;
             validTries++;
         }
+        delay(30);
     }
-    if(validTries == 0){
-        return(-1);
+    if (validTries == 0) return -1;
+    return totalDist / validTries;
+}
+
+Infra::Infra() {}
+
+void Infra::attach(uint8_t pin, float A, float B) {
+    this->pin = pin;
+    this->A = A;
+    this->B = B;
+    pinMode(this->pin, INPUT);
+}
+
+float Infra::read(int limMin, int limMax) {
+    float value = analogRead(this->pin);
+    if (value <= 0) return -1;
+    float d = A * pow(value, B);
+    if (d >= limMin && d <= limMax) return d;
+    return -1;
+}
+
+float Infra::average(int times, int limMin, int limMax) {
+    float totalDist = 0;
+    int validTries = 0;
+    for (int i = 0; i < times; i++) {
+        float valid = read(limMin, limMax);
+        if (valid != -1) {
+            totalDist += valid;
+            validTries++;
+        }
+        delay(10);
     }
-    dist /= validTries;
-    return(dist);
+    if (validTries == 0) return -1;
+    return totalDist / validTries;
 }
--- src/Sonic.h
+++ src/Sonic.h
@@ -1,7 +1,16 @@
 /*
-    * Sonic Library - Ultrasonic Sensor HC-SR04  
+    * Sonic Library - Ultrasonic Sensor HC-SR04 & IR Analog Sensor
     * Copyright (c) 2026 Jorge Leonardo Mendoza Hernández
-    * MIT License  
+    * 
+    * Licensed under the GNU General Public License, Version 3.0 (the "License");
+    * you may not use this file except in compliance with the License.
+    * You may obtain a copy of the License at
+    * 
+    *     https://gnu.org
+    * 
+    * Unless required by applicable law or agreed to in writing, software
+    * distributed under the License is distributed on an "AS IS" BASIS,
+    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 */
 
 #ifndef SONIC_H
@@ -14,13 +23,25 @@ class Sonic {
         void attach(uint8_t trig, uint8_t echo);
         long scream(int limMin = 3, int limMax = 800);
         long average(int times = 2, int limMin = 3, int limMax = 800);
+        static void globalISR();
     private:
-        uint8_t trig;
-        uint8_t echo;
-        volatile long start;
+        uint8_t trig, echo;
+        volatile uint32_t start;
         volatile long dist;
-        volatile bool ready;
-        volatile bool measuring;
+        volatile bool ready, measuring;
+        Sonic* next;
+        static Sonic* firstInstance;
+};
+
+class Infra {
+    public:
+        Infra();
+        void attach(uint8_t pin, float A = 12343.85, float B = -1.15);
+        float read(int limMin = 2, int limMax = 400);
+        float average(int times = 5, int limMin = 2, int limMax = 400);
+    private:
+        uint8_t pin;
+        float A, B;
 };
 
 #endif
